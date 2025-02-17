4f5016908d09f12a931aeba1d1808963cc0b70c4
PeterNgeno <peterkipsangngeno@gmail.com>
2025-02-17 23:04:52 +0300

    Create analytics.js

---
+++ firebase/analytics.js
@@ -0,0 +1,4 @@
+// Placeholder for any Firebase Analytics integration or helper functions.
+module.exports.trackEvent = (eventName, data) => {
+  console.log(`Event tracked: ${eventName}`, data);
+};
