0babc9fa0c68566d2cda4d5a181db3a22004a93e
PeterNgeno <peterkipsangngeno@gmail.com>
2025-02-18 22:06:31 +0300

    Delete routes directory

--- routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-// Route to send a notification (admin only)
-router.post('/send', authMiddleware.verifyAdmin, notificationController.sendNotification);
-
-module.exports = router;
